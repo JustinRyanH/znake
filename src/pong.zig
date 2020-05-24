f0f2252130a5c835e77edce767e039c724ff8a11
Justin Hurstwright <jryanhurst@gmail.com>
2020-05-24 15:19:44 -0700

    Draw to screen

--- src/pong.zig
+++ src/pong.zig
@@ -1,6 +1,75 @@
-pub const GameDrawBuffer = struct {
-    height: i32,
-    width: i32,
-    pitch: i32,
-    memory: *c_void,
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub const DebugData = struct {
+    x_offset: i32,
+    y_offset: i32,
+    tone_hz: u32,
+    sine_time: f32,
 };
+
+pub const GameData = struct {
+    initialized: bool = false,
+
+    permanent_storage: [*]u8,
+    permanent_storage_size: usize,
+
+    transient_storage: [*]u8,
+    transient_storage_Size: usize,
+};
+
+pub const GameDrawBuffer = struct {
+    height: u32,
+    width: u32,
+    pitch: u32,
+    memory: []u8,
+};
+
+// pub export fn GameUpdateDrawBuffer(arg_buffer: [*c]struct_GameDrawBuffer) void {
+//     var buffer = arg_buffer;
+//     var row: [*c]u8_5 = @ptrCast([*c]u8_5, @alignCast(@alignOf(u8_5), buffer.*.memory));
+//     {
+//         var green_shift: c_int = 0;
+//         while (green_shift < buffer.*.height) : (green_shift += 1) {
+//             var pixel: [*c]u32_7 = @ptrCast([*c]u32_7, @alignCast(@alignOf(u32_7), row));
+//             {
+//                 var blue_shift: c_int = 0;
+//                 while (blue_shift < buffer.*.width) : (blue_shift += 1) {
+//                     var blue: u8_5 = @bitCast(u8_5, @truncate(i8, (blue_shift)));
+//                     var green: u8_5 = @bitCast(u8_5, @truncate(i8, (green_shift)));
+//                     (blk: {
+//                         const ref = &pixel;
+//                         const tmp = ref.*;
+//                         ref.* += 1;
+//                         break :blk tmp;
+//                     }).?.* = @bitCast(u32_7, ((@bitCast(c_int, @as(c_uint, green)) << @intCast(@import("std").math.Log2Int(c_int), 8)) | @bitCast(c_int, @as(c_uint, blue))));
+//                 }
+//             }
+//             row += buffer.*.pitch;
+//         }
+//     }
+// }
+
+pub const Pixel = packed struct {
+    blue: u8,
+    green: u8,
+    red: u8,
+    padding: u8,
+};
+
+pub fn DebugFillBuffer(draw_buffer: *GameDrawBuffer) void {
+    assert(draw_buffer.memory.len == draw_buffer.height * draw_buffer.pitch);
+    var y_index: usize = 0;
+    var pixels = std.mem.bytesAsSlice(Pixel, draw_buffer.memory);
+    assert(pixels.len == draw_buffer.height * draw_buffer.width);
+
+    while (y_index < draw_buffer.height) {
+        const start = y_index * draw_buffer.width;
+        const end = start + draw_buffer.width;
+        for (pixels[start..end]) |*pixel| {
+            pixel.*.blue = 0xFF;
+            pixel.*.green = 0xFF;
+        }
+        y_index += 1;
+    }
+}
---
+++ src/win32_draw.zig
@@ -0,0 +1,111 @@
+const std = @import("std");
+const win32 = @import("win32.zig");
+const pong = @import("pong.zig");
+
+const GameDrawBuffer = pong.GameDrawBuffer;
+
+pub const BYTES_PER_PIXEL = 4;
+pub const Win32OffscreenBuffer = struct {
+    const BitmapInfoHeader = win32.BITMAPINFOHEADER;
+    const BitmapInfo = win32.BITMAPINFO;
+    const Self = @This();
+
+    info: BitmapInfo,
+    memory: [*]u8,
+    width: u32,
+    height: u32,
+    pitch: u32,
+
+    pub fn init(width: u32, height: u32) !Self {
+        if (win32.VirtualAlloc(
+            null,
+            @intCast(usize, width * height * BYTES_PER_PIXEL),
+            win32.MEM_COMMIT | win32.MEM_RESERVE,
+            win32.PAGE_READWRITE,
+        )) |memory| {
+            return Self{
+                .width = width,
+                .height = height,
+                .pitch = width * BYTES_PER_PIXEL,
+                .info = buildInfo(width, height),
+                .memory = @ptrCast([*]u8, memory),
+            };
+        }
+        return win32.WindowError.FailedToAllocateMemory;
+    }
+
+    pub fn resize(self: *Self, width: i32, height: i32) !Self {
+        if (!self.release()) {
+            return win32.WindowError.FailedToUnallocateMemory;
+        }
+        if (win32.VirtualAlloc(
+            null,
+            @intCast(usize, width * height * BYTES_PER_PIXEL),
+            win32.MEM_COMMIT | win32.MEM_RESERVE,
+            win32.PAGE_READWRITE,
+        )) |memory| {
+            return Self{
+                .width = width,
+                .height = height,
+                .pitch = width * BYTES_PER_PIXEL,
+                .info = header(width, height),
+                .memory = @ptrCast([*]u8, memory),
+            };
+        }
+        return win32.WindowError.FailedToAllocateMemory;
+    }
+
+    pub fn release(self: Self) !void {
+        if (!win32.VirtualFree(self.memory, 0, win32.MEM_RELEASE)) {}
+    }
+
+    pub fn gamebuffer(self: Self) GameDrawBuffer {
+        return GameDrawBuffer{
+            .height = self.height,
+            .width = self.width,
+            .pitch = self.pitch,
+            .memory = self.memory[0..(self.pitch * self.height)],
+        };
+    }
+
+    pub fn sync(self: Self, draw_buffer: *GameDrawBuffer) void {
+        draw_buffer.height = self.height;
+        draw_buffer.width = self.width;
+        draw_buffer.pitch = self.pitch;
+        draw_buffer.memory = self.memory[0..(self.pitch * self.height)];
+    }
+
+    pub fn blit(self: *Self, hdc: win32.HDC) bool {
+        // TODO(justin): Take in Window Information and center it,
+        // OR keep the aspect ratio the same and still center it
+        return win32.StretchDIBits(
+            hdc,
+            0,
+            0,
+            @intCast(i32, self.width),
+            @intCast(i32, self.height),
+            0,
+            0,
+            @intCast(i32, self.width),
+            @intCast(i32, self.height),
+            self.memory,
+            &self.info,
+            win32.DIB_RGB_COLORS,
+            win32.SRCCOPY,
+        ) != 0;
+    }
+
+    fn buildInfo(width: u32, height: u32) BitmapInfo {
+        const header = BitmapInfoHeader{
+            .biWidth = @intCast(i32, width),
+            .biHeight = -@intCast(i32, height),
+            .biPlanes = 1,
+            .biBitCount = 32,
+            .biCompression = win32.BI_RGB,
+        };
+        return BitmapInfo{
+            .bmiHeader = header,
+            .bmiColors = undefined,
+        };
+    }
+};
