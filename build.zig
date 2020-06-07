e7ffb6744fbbe518bf28580518cb7250fe4d0e42
Justin Hurstwright <jryanhurst@gmail.com>
2020-06-07 15:06:20 -0700

    Move builds to it's own directory

--- build.zig
+++ build.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const BuildExe = *std.build.LibExeObjStep;
 
+const build_root = "build";
+
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
@@ -12,10 +14,14 @@ pub fn build(b: *Builder) void {
         dll = b.addSharedLibrary("game", "src/pong.zig", b.version(0, 0, 1));
         dll.setTarget(target);
         dll.setBuildMode(mode);
+        dll.setOutputDir(build_root);
+        b.default_step.dependOn(&dll.step);
 
         exe = b.addExecutable("zpong", "src/win32_platform.zig");
         exe.setTarget(target);
         exe.setBuildMode(mode);
+        exe.setOutputDir(build_root);
+        b.default_step.dependOn(&exe.step);
 
         exe.linkSystemLibrary("c");
         exe.linkSystemLibrary("gdi32");
@@ -25,8 +31,6 @@ pub fn build(b: *Builder) void {
         @panic("Unimplemented Platform");
     }
 
-    dll.install();
-    exe.install();
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
 
