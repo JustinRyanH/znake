977a5428d526a6cdecf5983ff6dddd9c529d15c9
Justin Hurstwright <jryanhurst@gmail.com>
2020-11-15 16:49:03 -0800

    chore: Extract exe build

--- build.zig
+++ build.zig
@@ -1,20 +1,29 @@
 const std = @import("std");
 const builtin = @import("builtin");
+
 const Builder = std.build.Builder;
+const CrossTarget = std.zig.CrossTarget;
+const BuildMode = builtin.Mode;
+const LibExeObjStep = std.build.LibExeObjStep;
 
 
 const is_windows = std.Target.current.os.tag == .windows;
 
-pub fn build(b: *Builder) void {
-    const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
-
+pub fn buildExe(b: *Builder, target: CrossTarget, mode: BuildMode) *LibExeObjStep {
     const exe = b.addExecutable("sokol-zsnake", "src/platform_sokol.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackagePath("sokol", "src/sokol/sokol.zig");
     exe.addCSourceFile("src/sokol/sokol.c", &[_][]const u8{"-std=c99"});
     exe.linkSystemLibrary("c");
+    return exe;
+}
+
+pub fn build(b: *Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = buildExe(b, target, mode);
     exe.install();
 
     const cflags = [_][]const u8{"-std=c99"};
