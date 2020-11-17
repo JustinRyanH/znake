4e0c5dec4d4af95ebf650f92782d57fa9beb9ea8
Justin Hurstwright <jryanhurst@gmail.com>
2020-11-17 13:08:02 -0800

    chore: Show timw working

--- src/game.zig
+++ src/game.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const game = @import("game_types.zig");
+const Time = game.Time;
 
 export fn update_game(input: *game.Input, data: *game.Data) void {
-    std.debug.print("Frame: {}\n", .{ input.frame });
+    std.debug.print("Frame: {}\n", .{input.frame});
+    std.debug.print("Total Time: {}\n", .{Time.as_ms(input.time.since_start())});
 }
