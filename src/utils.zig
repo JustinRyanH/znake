ad02935edde6f93321ba5bf107056e304401e95a
Justin Hurstwright <justin.ryan.hurstwright@hey.com>
2020-11-18 07:44:51 -0800

    chore: Move over Memory size from original znakee

---
+++ src/utils.zig
@@ -0,0 +1,17 @@
+const builtin = @import("std").builtin;
+
+pub fn kilobytes(bytes: comptime usize) usize {
+    return bytes * 1024;
+}
+
+pub fn megabytes(bytes: comptime usize) usize {
+    return kilobytes(bytes) * 1024;
+}
+
+pub fn gigabytes(bytes: comptime usize) usize {
+    return megabytes(bytes) * 1024;
+}
+
+pub fn terabytes(bytes: comptime usize) usize {
+    return gigabytes(bytes) * 1024;
+}
