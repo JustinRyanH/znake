d85287dd0fcd1797a66141c5d7364778d7e16def
Justin Ryan Hurst <jryanhurst@gmail.com>
2020-11-17 21:32:05 -0800

    chore: Cleanup debug output

--- src/game.zig
+++ src/game.zig
@@ -3,6 +3,8 @@ const game = @import("game_types.zig");
 const Time = game.Time;
 
 export fn update_game(input: *game.Input, data: *game.Data) void {
-    std.debug.print("Frame: {}\n", .{input.frame});
-    std.debug.print("Total Time: {}\n", .{Time.as_ms(input.time.since_start())});
+    if (@mod(input.frame, 10) == 0) {
+        std.debug.print("Frame: {}\n", .{input.frame});
+        std.debug.print("Total Time: {}\n", .{Time.as_ms(input.time.since_start())});
+    }
 }
