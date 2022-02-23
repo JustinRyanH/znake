e950ecbe44919fcf5aca1fe12aa1736875dde3e7
Justin Hurstwright <jryanhurst@gmail.com>
2022-02-22 20:26:34 -0600

    refactor: Move State to game

--- src/game.zig
+++ src/game.zig
@@ -146,3 +146,127 @@ pub const StateSetup = struct {
     step_stride: u32,
     random: rand.Random,
 };
+
+pub const State = struct {
+    allocator: mem.Allocator,
+    random: rand.Random,
+    y_min: u8,
+    y_max: u8,
+    x_min: u8,
+    x_max: u8,
+    step_stride: u32,
+
+    frame: u32 = 0,
+    input: Input = .{},
+
+    next_tick: u32,
+    maybe_next_direction: Direction = .Up,
+    segments: SegmentList,
+    deadSegments: SegmentList,
+    fruit: Fruit = .{},
+    game_state: GameState = .GameOver,
+
+    pub fn updateInput(self: *State, input: Input) void {
+        self.input = input;
+    }
+
+    pub fn allocAndInit(allocator: mem.Allocator, config: StateSetup) *State {
+        var state = allocator.create(State) catch @panic("Could not Allocate Game Data");
+        state.* = .{
+            .y_min = config.y_min,
+            .y_max = config.y_max,
+            .x_min = config.x_min,
+            .x_max = config.x_max,
+            .step_stride = config.step_stride,
+            .next_tick = config.step_stride,
+            .allocator = allocator,
+            .random = config.random,
+            .segments = SegmentList.init(allocator),
+            .deadSegments = SegmentList.init(allocator),
+        };
+        return state;
+    }
+
+    pub fn shouldTick(self: *State) bool {
+        if (self.frame == self.next_tick) {
+            self.next_tick = self.frame + self.step_stride;
+            return true;
+        }
+        return false;
+    }
+
+    pub fn reset(self: *State) void {
+        self.frame = 0;
+        self.next_tick = self.step_stride;
+        self.segments.clearAndFree();
+        self.maybe_next_direction = .Up;
+
+        const x = (self.x_max - self.x_min) / 2 - 1;
+        const y = (self.y_max - self.y_min) / 2 - 1;
+        const StartPosition = Vec2{ .x = x, .y = y };
+        const starting_segment = Segment{ .direction = self.maybe_next_direction, .position = StartPosition };
+        const starting_tail = Segment{ .direction = self.maybe_next_direction, .position = starting_segment.position.add(Vec2{ .x = 0, .y = 1 }) };
+        self.addSegment(starting_segment);
+        self.addSegment(starting_tail);
+        self.game_state = .Play;
+        self.nextFruit();
+    }
+
+    pub fn willCollideWithSelf(self: *State) bool {
+        const snake_head = self.snakeHead();
+        const next_pos = snake_head.nextPosition();
+        for (self.segments.items[1..]) |segment| {
+            if (segment.nextPosition().equals(next_pos)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    pub fn nextFruit(self: *State) void {
+        self.fruit.pos = Vec2{
+            .x = self.random.intRangeLessThan(i32, self.x_min, self.x_max),
+            .y = self.random.intRangeLessThan(i32, self.y_min + 1, self.y_max),
+        };
+    }
+
+    pub fn snakeHead(self: *State) *Segment {
+        return &self.segments.items[0];
+    }
+
+    pub fn maybEat(self: *State) void {
+        const snake_head = self.snakeHead();
+        if (!self.fruit.overlaps(snake_head.position)) {
+            return;
+        }
+        self.fruit.pos = null;
+    }
+
+    pub fn updateSegments(self: *State) void {
+        const segments = self.segments.items;
+        var i: usize = segments.len;
+        while (i > 0) {
+            i -= 1;
+            const segment = segments[i];
+            const nextPosition = segment.nextPosition();
+            segments[i].position = nextPosition;
+            segments[i + 1].direction = segments[i].direction;
+        }
+    }
+
+    pub fn addSegment(self: *State, segment: Segment) void {
+        self.segments.append(segment) catch @panic("Cannot Grow Snake");
+    }
+
+    pub fn willBeOutOfBounds(self: *const State, segment: *Segment) bool {
+        const position = segment.position.add(segment.direction.to_vec2());
+        if (position.y < self.y_min or position.y > self.y_max) {
+            return true;
+        }
+        if (position.x < self.x_min or position.x > self.x_max) {
+            return true;
+        }
+        return false;
+    }
+};
