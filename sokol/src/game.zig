30adbb6bf0290c98aa6649711e410cac6f954382
Justin Hurstwright <jryanhurst@gmail.com>
2022-04-23 13:10:09 -0500

    ref: Add a collisoin struct

--- sokol/src/game.zig
+++ sokol/src/game.zig
@@ -248,6 +248,11 @@ pub const StateSetup = struct {
 };
 
 pub const SnakeGame = struct {
+    bounds: Bounds,
+    events: GameEvents,
+    fruit: Fruit,
+    head_direction: HeadDirection,
+    randoms: RandomGenerators,
     step_stride: u32,
 };
 
@@ -361,6 +366,15 @@ pub const State = struct {
             .step_stride = config.step_stride,
             .allocator = allocator,
         };
+        const snake_game = SnakeGame{
+            .step_stride = config.step_stride,
+            .bounds = bounds,
+            .events = events,
+            .randoms = randoms,
+            .fruit = Fruit{},
+            .head_direction = HeadDirection{},
+        };
+        state.registery.singletons().add(snake_game);
         state.registery.singletons().add(HeadDirection{});
         state.registery.singletons().add(Fruit{});
         state.registery.singletons().add(bounds);
