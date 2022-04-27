c2dd982ef57842b7eab19d7edd6f96b589fc38e1
Justin Hurstwright <jhurstwright@mavenlink.com>
2022-04-27 10:26:40 -0500

    ref: Move struct to Types

--- sokol/src/game.zig
+++ sokol/src/game.zig
@@ -111,35 +111,7 @@ pub const GameEvents = struct {
     }
 };
 
-pub const Vec2 = struct {
-    x: i32 = 0,
-    y: i32 = 0,
-
-    pub fn add(self: Vec2, other: Vec2) Vec2 {
-        return Vec2{
-            .x = self.x + other.x,
-            .y = self.y + other.y,
-        };
-    }
-
-    pub fn sub(self: Vec2, other: Vec2) Vec2 {
-        return Vec2{
-            .x = self.x - other.x,
-            .y = self.y - other.y,
-        };
-    }
-
-    pub fn scalar(self: Vec2, by: i32) Vec2 {
-        return Vec2{
-            .x = self.x * by,
-            .y = self.y * by,
-        };
-    }
-
-    pub fn equals(self: Vec2, other: Vec2) bool {
-        return self.x == other.x and self.y == other.y;
-    }
-};
+pub const Vec2 = Types.Vec2;
 
 pub const Direction = enum {
     Up,
--- sokol/src/types.zig
+++ sokol/src/types.zig
@@ -4,3 +4,33 @@ pub const Bounds = struct {
     y_min: i32,
     y_max: i32,
 };
+
+pub const Vec2 = struct {
+    x: i32 = 0,
+    y: i32 = 0,
+
+    pub fn add(self: Vec2, other: Vec2) Vec2 {
+        return Vec2{
+            .x = self.x + other.x,
+            .y = self.y + other.y,
+        };
+    }
+
+    pub fn sub(self: Vec2, other: Vec2) Vec2 {
+        return Vec2{
+            .x = self.x - other.x,
+            .y = self.y - other.y,
+        };
+    }
+
+    pub fn scalar(self: Vec2, by: i32) Vec2 {
+        return Vec2{
+            .x = self.x * by,
+            .y = self.y * by,
+        };
+    }
+
+    pub fn equals(self: Vec2, other: Vec2) bool {
+        return self.x == other.x and self.y == other.y;
+    }
+};
